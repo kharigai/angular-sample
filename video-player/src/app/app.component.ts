2427167d8145759c4af55b20aef1d44aa00c2b8d
kharigai <kharigai@gmail.com>
2023-09-13 11:19:44 +0900

    Add Loop Video checkbox

--- video-player/src/app/app.component.ts
+++ video-player/src/app/app.component.ts
@@ -12,4 +12,11 @@ export class AppComponent {
   updateVideoUrl(newUrl: string): void {
     this.videoUrl = newUrl;
   }
+
+  // video loop 設定を変更
+  loopVideo: boolean = false;
+
+  toggleLoop(): void {
+    this.loopVideo = !this.loopVideo; // loopの状態を切り替える
+  }
 }
